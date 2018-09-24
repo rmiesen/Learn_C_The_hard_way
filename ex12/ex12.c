0a69fea1b5d0fd87b02a8e58354e0ab349772bee
Robert Miesen <robert.miesen@gmail.com>
2018-09-24 02:29:27 -0700

    Added missing newline to printf statement

--- ex12/ex12.c
+++ ex12/ex12.c
@@ -22,10 +22,10 @@ int main(int argc, char *argv[])
         printf("The size of name (char[]): %ld\n", sizeof(name));
         printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
 
-        printf("The size of full_name (char[]): %ld", sizeof(full_name));
+        printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
         printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
 
         printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
-        
+
         return 0;
 }
