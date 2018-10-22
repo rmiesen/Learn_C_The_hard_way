316e0f70378032eda9ec75835cdfe81a978c24cc
Robert Miesen <robert.miesen@intel.com>
2018-10-22 11:56:15 -0700

    Added more parts of Exercise 17

--- ex17/ex17.c
+++ ex17/ex17.c
@@ -40,13 +40,86 @@ void Address_print(struct Address *addr)
         printf("%d %s %s\n", addr->id, addr->name, addr->email);
 }
 
-struct Connection *Database_open( sruct Connection *conn)
+struct Connection *Database_load( sruct Connection *conn)
 {
         int rc = fread(conn->db, sizeof(struct Database),  1, conn->file);
         if (rc != 1) {
                 die("Failed to load database.");
         }
 }
+
+struct Connection *Database_open(aconst char *filename, const char mode)
+{
+        struct connection *conn = malloc(sizeof(struct Connection));
+        if (!conn) {
+                die("Memory error");
+        }
+
+        conn->db = malloc(sizeof(struct Database));
+
+        if (!conn->db) {
+                die("Memory error");
+        }
+
+        if (mode == 'c') {
+                conn->file = fopen(filename, "w");
+        } else {
+                conn->file = fopen(filename, "r+");
+
+                if (conn->file) {
+                        Database_load(conn);
+                }
+        }
+
+        if (!conn->file) {
+                die("Failed to open the file");
+        }
+
+}
+
+void Database_close(struct Connection *conn)
+{
+        if (conn) {
+                if (conn->file){
+                        fclose(conn->file);
+                }
+
+                if (conn->db) {
+                        free(conn->db)
+                }
+
+                free(conn);
+        }
+}
+
+void Database_write(struct Connection *conn)
+{
+        rewind(conn->file);
+
+        int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+        if (rc != 1) {
+                die("Failed to write database.");
+        }
+
+        rc = fflush(conn->file);
+        if (rc == -1) {
+                die("Cannot flush database");
+        }
+}
+
+void Database_create(struct Connection *conn)
+{
+        int i = 0;
+
+        for (i = 0 ; i < MAX_ROWS; ++i) {
+                // make a prototype to initialize it
+                struct Address  addr = {.id = i, .set = 0};
+                // This should be a C_ctor.  - rmiesen
+                conn->db->rows[i] = addr;
+        }
+}
+
+//LEFT_OFF_HERE: Continue adding stuff at Database_set
 
 int main (int argc, char *argv[])
 {
